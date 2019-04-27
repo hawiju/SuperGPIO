f31dbdc73a2d68a8945a348367b9870ef69032b7
hawiju <31375101+hawiju@users.noreply.github.com>
2019-04-27 16:07:11 +0300

    Add files via upload

--- supergpio.hpp
+++ supergpio.hpp
@@ -1,4 +1,5 @@
 #include "stm32f4xx.h"
+//#include "stm32f7xx.h"
 namespace gpio {
 
 enum class mode :int {
@@ -21,6 +22,14 @@ enum class pins:int {
 	pin0 = 0, pin1, pin2, pin3, pin4, pin5, pin6, pin7, pin8, pin9, pin10, pin11, pin12, pin13, pin14, pin15
 };
 
+struct Pin{
+	uint8_t pin
+;
+	//3 bytes go here
+	GPIO_TypeDef* port;
+};
+
+#if __cpp_fold_expressions >= 201411 //true for GCC>=6
 template<uint8_t ... pins>
 inline void init(GPIO_TypeDef* port, mode m = mode::input, type t = type::PP, speed s = speed::low, pupd p = pupd::nopupd, af a = af::af0);
 
@@ -33,8 +42,18 @@ inline void reset(GPIO_TypeDef* port);
 template<uint32_t ... pins>
 inline void toggle(GPIO_TypeDef* port);
 
+#endif
+
+inline void init(Pin pin, mode m = mode::input, type t = type::PP, speed s = speed::low, pupd p = pupd::nopupd, af a = af::af0);
+inline void set(Pin p);
+inline void reset(Pin p);
+inline void toggle(Pin p);
+
+
 }
 
+
+#if __cpp_fold_expressions >= 201411
 template<uint8_t ... pins>
 inline void gpio::init(GPIO_TypeDef * port, mode m, type t, speed s, pupd p, af a) {
 
@@ -71,7 +90,7 @@ inline void gpio::init(GPIO_TypeDef * port, mode m, type t, speed s, pupd p, af 
 	((temp1|=(0b1<<(pins))), ... );
 	port->OTYPER &= ~(temp1);
 	temp1 = 0;
-	((temp1|=(int(t)<<(2*pins))), ... );
+	((temp1|=(int(t)<<(pins))), ... );
 	port->OTYPER |= temp1;
 	temp1 = 0;
 	((temp1|=(0b11<<(2*pins))), ... );
@@ -116,4 +135,57 @@ inline void gpio::reset(GPIO_TypeDef* port) {
 	uint32_t t = 0;
 	((t |= 1<<(pins+16)), ...);
 	port->BSRR = t;
+}
+
+#endif
+
+inline void gpio::init(Pin pin, mode m , type t, speed s, pupd p , af a){
+
+	if (pin.port == GPIOA)
+		RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+	if (pin.port == GPIOB)
+		RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+	if (pin.port == GPIOC)
+		RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
+	if (pin.port == GPIOD)
+		RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
+	if (pin.port == GPIOE)
+		RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN;
+	if (pin.port == GPIOF)
+		RCC->AHB1ENR |= RCC_AHB1ENR_GPIOFEN;
+	if (pin.port == GPIOG)
+		RCC->AHB1ENR |= RCC_AHB1ENR_GPIOGEN;
+	if (pin.port == GPIOH)
+		RCC->AHB1ENR |= RCC_AHB1ENR_GPIOHEN;
+	if (pin.port == GPIOI)
+		RCC->AHB1ENR |= RCC_AHB1ENR_GPIOIEN;
+//	if (pin.port == GPIOJ)                    //for big F7
+//		RCC->AHB1ENR |= RCC_AHB1ENR_GPIOJEN;
+//	if (pin.port == GPIOK)
+//		RCC->AHB1ENR |= RCC_AHB1ENR_GPIOKEN;
+
+
+	pin.port->MODER &= ~((0b11<<(2*pin.pin)));
+	pin.port->MODER |= int(m)<<(2*pin.pin);
+
+	pin.port->OTYPER &= ~((0b1<<(pin.pin)));
+	pin.port->OTYPER |= (int(t)<<(pin.pin));
+
+	pin.port->OSPEEDR &= ~((0b11<<(2*pin.pin)));
+	pin.port->OSPEEDR |= (int(s)<<(2*pin.pin));
+	
+	pin.port->PUPDR &= ~((0b11<<(2*pin.pin)));
+	pin.port->PUPDR |= (int(p)<<(2*pin.pin));
+	
+	pin.port->AFR[pin.pin<8?0:1]&= ~(0b11<<(4*(pin.pin-(pin.pin<8?0:8))));
+	pin.port->AFR[pin.pin<8?0:1]|= (int(a)<<(4*(pin.pin-(pin.pin<8?0:8))));
+}
+inline void gpio::set(Pin p){
+	p.port->BSRR=1<<p.pin;
+}
+inline void gpio::reset(Pin p){
+	p.port->BSRR=1<<(p.pin+16);
+}
+inline void gpio::toggle(Pin p){
+	p.port->ODR^=(1<<(p.pin));
 }
